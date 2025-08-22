10b0c23c5cd19ebbf26fd315f604c86f403ef35c
Michał Pełka <michal.pelka@robotec.ai>
2025-08-22 16:09:17 +0200

    Added IDL based service discovery
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

--- src/simulation_widget.h
+++ src/simulation_widget.h
@@ -37,13 +37,14 @@
 #include <simulation_interfaces/srv/set_simulation_state.hpp>
 #include <simulation_interfaces/srv/spawn_entity.hpp>
 #include <simulation_interfaces/srv/step_simulation.hpp>
-
+#include <QComboBox>
 namespace Ui
 {
     class simWidgetUi;
 }
 namespace q_simulation_interfaces
 {
+    constexpr const char IDLPropertyName[]  = "IDLPropertyName";
     const char InteractiveMarkerNamespaceValue[] = "simulation_interfaces_panel";
     class SimulationWidget : public QWidget
     {
@@ -61,6 +62,7 @@ namespace q_simulation_interfaces
         void hideEvent(QHideEvent* event) override;
         void showEvent(QShowEvent* event) override;
 
+        void onShowServicesTab();
         void GetSpawnables();
         void SpawnButton();
         void GetAllEntities();
@@ -106,6 +108,11 @@ namespace q_simulation_interfaces
 
         // Vector to hold all service interfaces of created services
         std::vector<std::shared_ptr<ServiceInterface>> serviceInterfaces_;
+
+        std::map<std::string, QComboBox*> serviceComboBoxesByIDLType_;
+        std::map<std::string, QLabel*> serviceLabelsByIDLType_;
+        std::map<std::string, std::shared_ptr<ServiceInterface>> serviceInterfacesByIDLType_;
+
         QTimer* timer_; //! Timer for periodic updates
 
         std::shared_ptr<interactive_markers::InteractiveMarkerServer> interactiveMarkerServer_;
