33761b938b479826b6e7a8a0e166fa5e0beccd5b
Jan Hanca <jan.hanca@robotec.ai>
2025-10-21 15:31:19 +0200

    Self code-review: fix empty axis
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

---
+++ src/quaternion_utils.h
@@ -0,0 +1,42 @@
+/* Copyright 2025, Robotec.ai sp. z o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Vector3.h>
+
+inline tf2::Quaternion AxisAngleToQuaternion(const tf2::Vector3& axis, const double angle)
+{
+    tf2::Quaternion q;
+
+    if (axis.length() > 1e-6)
+    {
+        q = tf2::Quaternion(axis, angle);
+        q.normalize();
+    }
+    else
+    {
+        // Default to no rotation if vector is zero
+        q.setValue(0.0, 0.0, 0.0, 1.0);
+    }
+
+    if (std::isnan(q.x()) || std::isnan(q.y()) || std::isnan(q.z()) || std::isnan(q.w()))
+    {
+        q.setValue(0.0, 0.0, 0.0, 1.0);
+    }
+
+    return q;
+}
